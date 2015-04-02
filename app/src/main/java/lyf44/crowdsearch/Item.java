8d4986894890bdadd39ea698c9e48786ccdba8d3
lyf44 <luyunfan44@gmail.com>
2015-04-02 20:17:43 +0800

    prilimary UI design

---
+++ app/src/main/java/lyf44/crowdsearch/Item.java
@@ -0,0 +1,18 @@
+package lyf44.crowdsearch;
+
+/**
+ * Created by lyf44 on 19/3/2015.
+ */
+public class Item {
+
+    protected String name;
+    protected String location;
+    protected String colour;
+    protected Object User;
+    protected String brand;
+    protected String remark;
+    protected String photo;
+    protected int point;
+
+
+}
---
+++ app/src/main/java/lyf44/crowdsearch/User.java
@@ -0,0 +1,13 @@
+package lyf44.crowdsearch;
+
+/**
+ * Created by lyf44 on 19/3/2015.
+ */
+public class User {
+
+    protected String userid;
+    protected String password;
+    protected String email;
+    protected String
+
+}
